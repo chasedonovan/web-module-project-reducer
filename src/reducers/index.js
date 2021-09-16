67f88d5cb841dc39702a15cb7e2cc12e47d8a97c
Chase Donovan <chase.donovan@aol.com>
2021-09-16 14:38:12 -0700

    mvp complete and calculator works perfectly

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import {APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY, CHANGE_MEMORY, RECALL_MEMORY, CLEAR_MEMORY } from './../actions';
 
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 0,
+    operation: "+",
+    memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -19,11 +19,11 @@ const calculateResult = (num1, num2, operation) => {
 
 const reducer = (state, action) => {
     switch(action.type) {
-        case(ADD_ONE):
+        case(CLEAR_MEMORY):
             return({
-                ...state,
-                total: state.total + 1
-            });
+            ...state, 
+            memory: 0
+        });
 
         case(APPLY_NUMBER):
             return ({ 
@@ -36,6 +36,21 @@ const reducer = (state, action) => {
                 ...state,
                 operation: action.payload
             });
+
+            case(RECALL_MEMORY):
+            return({ ...state, 
+                total: calculateResult(state.total, state.memory, state.operation)
+            })
+        case (CHANGE_MEMORY):
+            return({
+                ...state, 
+                memory: state.total
+            })
+        case(CLEAR_DISPLAY):
+            return({
+                ...state, 
+                total: 0
+            })
             
         default:
             return state;
